94fb5b540ad0bd780534cc23930e7b5af3452736
benNjogu <kamandemachafi@gmail.com>
2022-07-11 18:50:50 +0300

    selected buttons

--- options.js
+++ options.js
@@ -1,3 +1,10 @@
 //select elements
 const options = document.querySelector(".options");
 const gameOverElement = document.querySelector(".gameover");
+
+//select buttons
+const computerBtn = options.querySelector(".computer");
+const friendBtn = options.querySelector(".friend");
+const xBtn = options.querySelector(".x");
+const oBtn = options.querySelector(".o");
+const play = options.querySelector(".play");
