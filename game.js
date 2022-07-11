d69040a52882eedf1ecf90ea3265591ab5360931
benNjogu <kamandemachafi@gmail.com>
2022-07-11 20:10:01 +0300

    drawing the board

--- game.js
+++ game.js
@@ -8,4 +8,17 @@ function init(player, OPPONENT) {
   const COLUMN = 3;
   const ROW = 3;
   const SPACE_SIZE = 150;
+
+  //draw the board
+  function drawBoard() {
+    for (let i = 0; i < ROW; i++) {
+      board[i] = [];
+      for (let j = 0; j < COLUMN; j++) {
+        ctx.strokeStyle = "#000";
+        ctx.strokeRect(j * SPACE_SIZE, i * SPACE_SIZE, SPACE_SIZE, SPACE_SIZE);
+      }
+    }
+  }
+
+  drawBoard();
 }
