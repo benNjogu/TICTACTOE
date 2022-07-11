92b38b609eb11da5782497b7d814bb2571276088
benNjogu <kamandemachafi@gmail.com>
2022-07-11 20:00:32 +0300

    added board variables

--- game.js
+++ game.js
@@ -2,4 +2,10 @@ function init(player, OPPONENT) {
   //select canvas
   const canvas = document.getElementById("cvs");
   const ctx = canvas.getContext("2d");
+
+  //board variables
+  let board = [];
+  const COLUMN = 3;
+  const ROW = 3;
+  const SPACE_SIZE = 150;
 }
