8f74f1305b56798ce14f5de66b06b6df41823054
benNjogu <kamandemachafi@gmail.com>
2022-07-11 20:31:25 +0300

    on players click

--- game.js
+++ game.js
@@ -21,4 +21,15 @@ function init(player, OPPONENT) {
   }
 
   drawBoard();
+
+  //on player's click
+  canvas.addEventListener("click", function (event) {
+    //x and y position of mouse click relative to our canvas
+    let X = event.clientX - canvas.getBoundingClientRect().x;
+    let Y = event.clientY - canvas.getBoundingClientRect().y;
+
+    //we calculate i and j of the clicked space
+    let i = Math.floor(Y / SPACE_SIZE);
+    let j = Math.floor(X / SPACE_SIZE);
+  });
 }
