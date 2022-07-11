7db3602fbd70fdb68dce3cd0d936622a69296031
benNjogu <kamandemachafi@gmail.com>
2022-07-11 19:05:46 +0300

    added player and opponent variables and values

--- options.js
+++ options.js
@@ -9,21 +9,29 @@ const xBtn = options.querySelector(".x");
 const oBtn = options.querySelector(".o");
 const play = options.querySelector(".play");
 
+//some variables to store users options
+let OPPONENT;
+const player = new Object;
+
 //add an eventlistener to each button
 computerBtn.addEventListener("click", function(){
-
+    OPPONENT = "computer";
 });
 
 friendBtn.addEventListener("click", function(){
-
+    OPPONENT = "friend";
 });
 
 xBtn.addEventListener("click", function(){
-
+    player.man = "X";
+    player.computer = "O";
+    player.friend = "O";
 });
 
 oBtn.addEventListener("click", function(){
-
+    player.man = "O";
+    player.computer = "X";
+    player.friend = "X";
 });
 
 play.addEventListener("click", function(){
