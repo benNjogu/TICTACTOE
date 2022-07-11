4b8c0df83f8fe738858bae6d6630f275aa613607
benNjogu <kamandemachafi@gmail.com>
2022-07-11 18:58:52 +0300

    added eventlisteners to buttons

--- options.js
+++ options.js
@@ -8,3 +8,24 @@ const friendBtn = options.querySelector(".friend");
 const xBtn = options.querySelector(".x");
 const oBtn = options.querySelector(".o");
 const play = options.querySelector(".play");
+
+//add an eventlistener to each button
+computerBtn.addEventListener("click", function(){
+
+});
+
+friendBtn.addEventListener("click", function(){
+
+});
+
+xBtn.addEventListener("click", function(){
+
+});
+
+oBtn.addEventListener("click", function(){
+
+});
+
+play.addEventListener("click", function(){
+
+});
